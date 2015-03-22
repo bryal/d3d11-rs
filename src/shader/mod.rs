5a3fc6a41d5c196d4e8ecd1844cb6b753139aacd
bryal <96.bryal@gmail.com>
2015-03-23 00:46:17 +0100

    New mod shader, with all enumerations

---
+++ src/shader/mod.rs
@@ -0,0 +1,35 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2015 Johan Johansson
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+//! Several API elements to help create and manage programmable shaders
+//!
+//! # References
+//! [Shader Reference, MSDN]
+//! (https://msdn.microsoft.com/en-us/library/windows/desktop/ff476160(v=vs.85).aspx)
+
+pub use self::enumerations::*;
+// pub use self::structures::*;
+// pub use self::interfaces::*;
+
+pub mod enumerations;
+// pub mod structures;
+// pub mod interfaces;
